8d53eba2b68e36e98fd0c8215232a6e5c91fbb6d
pxqing <panxiuqing@gmail.com>
2018-05-09 09:18:49 +0800

    Initial Commit

---
+++ uWebSocket/uWebSocket.h
@@ -0,0 +1,19 @@
+//
+//  uWebSocket.h
+//  uWebSocket
+//
+//  Created by 潘秀清 on 2018/5/9.
+//  Copyright © 2018年 潘秀清. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for uWebSocket.
+FOUNDATION_EXPORT double uWebSocketVersionNumber;
+
+//! Project version string for uWebSocket.
+FOUNDATION_EXPORT const unsigned char uWebSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <uWebSocket/PublicHeader.h>
+
+
